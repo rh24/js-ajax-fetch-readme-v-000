7763e1beea611b4427665871f1f8e0ab69ed463f
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-27 16:25:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '36463e85db2472cb5be79aa1ea86aaaac9685423'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
